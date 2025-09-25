5cca71adc0b9be8e4dcc2cf8bc488a1e1a0500c3
Averym31 <averym31@nycstudents.net>
2025-09-25 08:40:29 -0400

    9/25/2025 8:40am

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("-----------------------------------------|");
 
         System.out.print("Enter your bill here ($): ");
         double initialBill = scan.nextDouble();
         initialBill = Math.round(initialBill * 100.00) / 100.00;
-        System.out.println("-----------------------------------------|");
 
         System.out.print("Enter your tip percentage here (%): ");
         int tipPercent = scan.nextInt();
-        System.out.println("-----------------------------------------|");
 
         System.out.print("Enter the number of people here: ");
         int numPeople = scan.nextInt();
-        System.out.println("-----------------------------------------|");
 
-        double totalBill = initialBill+(initialBill*0.01*tipPercent);
+        double totalTip = initialBill*0.01*tipPercent;
+        totalTip = Math.round(totalTip * 100.00) / 100.00;
+        double totalBill = initialBill+totalTip;
         totalBill = Math.round(totalBill * 100.00) / 100.00;
+        double tipPerson = totalTip/numPeople;
+        tipPerson = Math.round(tipPerson * 100.00) / 100.00;
+        double billPerson = totalBill/numPeople;
+        billPerson = Math.round(billPerson * 100.00) / 100.00;
 
-        System.out.println(totalBill);
+        System.out.println("----------------------------------------");
+
+        System.out.print("Total tip amount: $");
+        System.out.printf("%.2f%n", totalTip);
+        System.out.print("Total bill cost: $");
+        System.out.printf("%.2f%n", totalBill);
+        System.out.print("Tip per person: $");
+        System.out.printf("%.2f%n", tipPerson);
+        System.out.print("Total bill per person: $");
+        System.out.printf("%.2f%n", billPerson);
     }
 }
